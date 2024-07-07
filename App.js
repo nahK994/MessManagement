b9ef066ffef211b89f3a9a78bf2e81b283276899
nahK994 <nkskl6@gmail.com>
2024-07-07 23:05:48 +0600

    first app

--- App.js
+++ App.js
@@ -1,20 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+  const [text, setText] = useState('')
+  const [enteredText, setEnteredText] = useState('')
+
+  function onChangeText(newText) {
+    setText(newText)
+  }
+
+  function onButtonPress() {
+    setEnteredText([...enteredText, text])
+    setText('')
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={style.main}>
+      <View style={style.input}>
+        <TextInput placeholder='Type here.....' style={style.textInput} onChangeText={onChangeText} value={text} />
+        <Button title='Click' style={style.button} onPress={onButtonPress} />
+      </View>
+      <View style={style['text-alignment']}>
+        {
+          enteredText.map(item => <Text style={style.item}>{item}</Text>)
+        }
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+const style = StyleSheet.create({
+  'main': {
+    flexDirection: 'column',
+    height: '100%',
     justifyContent: 'center',
+    alignItems: 'flex-start',
+    padding: 20,
+    rowGap: 12
   },
-});
+  'input': {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    columnGap: 12
+  },
+  'textInput': {
+    borderColor: 'green',
+    borderWidth: 1,
+    flex: 2,
+    borderRadius: 20,
+    padding: 10
+  },
+  'button': {
+    flex: 1
+  },
+  'text-alignment': {
+    flexDirection: 'row',
+    gap: 12,
+    flexWrap: 'wrap'
+  },
+  'item': {
+    borderRadius: 12,
+    backgroundColor: 'steelblue',
+    padding: 6,
+    color: 'white'
+  }
+})
