7fe3bf27d9d1e2561983798344f77f8033857c83
nahK994 <nkskl6@gmail.com>
2024-07-09 15:17:21 +0600

    update

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Button, TextInput, Text } from 'react-native';
+import { StyleSheet, View, TouchableOpacity, TextInput, Text, ScrollView, KeyboardAvoidingView, Platform } from 'react-native';
 import Output from './components/output';
 
 export default function App() {
@@ -24,153 +24,165 @@ export default function App() {
   };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.heading}>Expense Calculator</Text>
-      <View style={styles.inputContainer}>
-        <Text style={styles.label}>Utility Bills</Text>
-        <TextInput
-          value={electricityBill}
-          onChangeText={(text) => setElectricityBill(text)}
-          keyboardType='numeric'
-          placeholder='Electricity Bill'
-          style={styles.input}
-        />
-        <TextInput
-          value={gasBill}
-          onChangeText={(text) => setGasBill(text)}
-          keyboardType='numeric'
-          placeholder='Gas Bill'
-          style={styles.input}
-        />
-        <TextInput
-          value={internetBill}
-          onChangeText={(text) => setInternetBill(text)}
-          keyboardType='numeric'
-          placeholder='Internet Bill'
-          style={styles.input}
-        />
-        <TextInput
-          value={khalaBill}
-          onChangeText={(text) => setKhalaBill(text)}
-          keyboardType='numeric'
-          placeholder='Khala Bill'
-          style={styles.input}
-        />
-        <TextInput
-          value={othersBill}
-          onChangeText={(text) => setOthersBill(text)}
-          keyboardType='numeric'
-          placeholder='Others'
-          style={styles.input}
-        />
-      </View>
-
-      <View style={styles.memberContainer}>
-        <Text style={styles.label}>Members Expenses</Text>
-        <View style={styles.memberRow}>
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      keyboardVerticalOffset={Platform.OS === 'ios' ? 64 : 0} // Adjust the offset if necessary
+    >
+      <ScrollView contentContainerStyle={styles.scrollContainer}>
+        <Text style={styles.heading}>Expense Calculator</Text>
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>Utility Bills</Text>
           <TextInput
-            value={khanRent}
-            onChangeText={(text) => setKhanRent(text)}
+            value={electricityBill}
+            onChangeText={(text) => setElectricityBill(text)}
             keyboardType='numeric'
-            placeholder="Khan's Rent"
-            style={styles.memberInput}
+            placeholder='Electricity Bill'
+            style={styles.input}
           />
           <TextInput
-            value={khanBazar}
-            onChangeText={(text) => setKhanBazar(text)}
+            value={gasBill}
+            onChangeText={(text) => setGasBill(text)}
             keyboardType='numeric'
-            placeholder="Khan's Bazar"
-            style={styles.memberInput}
+            placeholder='Gas Bill'
+            style={styles.input}
           />
           <TextInput
-            value={khanMeal}
-            onChangeText={(text) => setKhanMeal(text)}
+            value={internetBill}
+            onChangeText={(text) => setInternetBill(text)}
             keyboardType='numeric'
-            placeholder="Khan's Meal"
-            style={styles.memberInput}
+            placeholder='Internet Bill'
+            style={styles.input}
+          />
+          <TextInput
+            value={khalaBill}
+            onChangeText={(text) => setKhalaBill(text)}
+            keyboardType='numeric'
+            placeholder='Khala Bill'
+            style={styles.input}
+          />
+          <TextInput
+            value={othersBill}
+            onChangeText={(text) => setOthersBill(text)}
+            keyboardType='numeric'
+            placeholder='Others'
+            style={styles.input}
           />
         </View>
 
-        <View style={styles.memberRow}>
-          <TextInput
-            value={rajRent}
-            onChangeText={(text) => setRajRent(text)}
-            keyboardType='numeric'
-            placeholder="Raj's Rent"
-            style={styles.memberInput}
-          />
-          <TextInput
-            value={rajBazar}
-            onChangeText={(text) => setRajBazar(text)}
-            keyboardType='numeric'
-            placeholder="Raj's Bazar"
-            style={styles.memberInput}
-          />
-          <TextInput
-            value={rajMeal}
-            onChangeText={(text) => setRajMeal(text)}
-            keyboardType='numeric'
-            placeholder="Raj's Meal"
-            style={styles.memberInput}
-          />
+        <View style={styles.memberContainer}>
+          <Text style={styles.label}>Members Expenses</Text>
+          <View style={styles.memberRow}>
+            <TextInput
+              value={khanRent}
+              onChangeText={(text) => setKhanRent(text)}
+              keyboardType='numeric'
+              placeholder="Khan's Rent"
+              style={styles.memberInput}
+            />
+            <TextInput
+              value={khanBazar}
+              onChangeText={(text) => setKhanBazar(text)}
+              keyboardType='numeric'
+              placeholder="Khan's Bazar"
+              style={styles.memberInput}
+            />
+            <TextInput
+              value={khanMeal}
+              onChangeText={(text) => setKhanMeal(text)}
+              keyboardType='numeric'
+              placeholder="Khan's Meal"
+              style={styles.memberInput}
+            />
+          </View>
+
+          <View style={styles.memberRow}>
+            <TextInput
+              value={rajRent}
+              onChangeText={(text) => setRajRent(text)}
+              keyboardType='numeric'
+              placeholder="Raj's Rent"
+              style={styles.memberInput}
+            />
+            <TextInput
+              value={rajBazar}
+              onChangeText={(text) => setRajBazar(text)}
+              keyboardType='numeric'
+              placeholder="Raj's Bazar"
+              style={styles.memberInput}
+            />
+            <TextInput
+              value={rajMeal}
+              onChangeText={(text) => setRajMeal(text)}
+              keyboardType='numeric'
+              placeholder="Raj's Meal"
+              style={styles.memberInput}
+            />
+          </View>
+
+          <View style={styles.memberRow}>
+            <TextInput
+              value={riajRent}
+              onChangeText={(text) => setRiajRent(text)}
+              keyboardType='numeric'
+              placeholder="Riaj's Rent"
+              style={styles.memberInput}
+            />
+            <TextInput
+              value={riajBazar}
+              onChangeText={(text) => setRiajBazar(text)}
+              keyboardType='numeric'
+              placeholder="Riaj's Bazar"
+              style={styles.memberInput}
+            />
+            <TextInput
+              value={riajMeal}
+              onChangeText={(text) => setRiajMeal(text)}
+              keyboardType='numeric'
+              placeholder="Riaj's Meal"
+              style={styles.memberInput}
+            />
+          </View>
         </View>
 
-        <View style={styles.memberRow}>
-          <TextInput
-            value={riajRent}
-            onChangeText={(text) => setRiajRent(text)}
-            keyboardType='numeric'
-            placeholder="Riaj's Rent"
-            style={styles.memberInput}
-          />
-          <TextInput
-            value={riajBazar}
-            onChangeText={(text) => setRiajBazar(text)}
-            keyboardType='numeric'
-            placeholder="Riaj's Bazar"
-            style={styles.memberInput}
-          />
-          <TextInput
-            value={riajMeal}
-            onChangeText={(text) => setRiajMeal(text)}
-            keyboardType='numeric'
-            placeholder="Riaj's Meal"
-            style={styles.memberInput}
-          />
-        </View>
-      </View>
+        <TouchableOpacity style={styles.button} onPress={handleToggleModal}>
+          <Text style={styles.buttonText}>Calculate</Text>
+        </TouchableOpacity>
 
-      <Button title='Calculate' onPress={handleToggleModal} />
-
-      <Output
-        visibility={isOpen}
-        setVisibility={setIsOpen}
-        electricityBill={electricityBill}
-        gasBill={gasBill}
-        internetBill={internetBill}
-        khalaBill={khalaBill}
-        othersBill={othersBill}
-        khanRent={khanRent}
-        khanBazar={khanBazar}
-        khanMeal={khanMeal}
-        rajRent={rajRent}
-        rajBazar={rajBazar}
-        rajMeal={rajMeal}
-        riajRent={riajRent}
-        riajBazar={riajBazar}
-        riajMeal={riajMeal}
-      />
-    </View>
+        <Output
+          visibility={isOpen}
+          setVisibility={setIsOpen}
+          electricityBill={electricityBill}
+          gasBill={gasBill}
+          internetBill={internetBill}
+          khalaBill={khalaBill}
+          othersBill={othersBill}
+          khanRent={khanRent}
+          khanBazar={khanBazar}
+          khanMeal={khanMeal}
+          rajRent={rajRent}
+          rajBazar={rajBazar}
+          rajMeal={rajMeal}
+          riajRent={riajRent}
+          riajBazar={riajBazar}
+          riajMeal={riajMeal}
+        />
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#e3f2fd',
+  },
+  scrollContainer: {
+    flexGrow: 1,
     justifyContent: 'center',
     alignItems: 'center',
     padding: 20,
-    backgroundColor: '#f0f0f0',
+    marginTop: 32
   },
   heading: {
     fontSize: 24,
@@ -197,6 +209,7 @@ const styles = StyleSheet.create({
     padding: 10,
     marginBottom: 10,
     width: '100%',
+    backgroundColor: 'white'
   },
   memberRow: {
     flexDirection: 'row',
@@ -211,6 +224,24 @@ const styles = StyleSheet.create({
     padding: 10,
     flex: 1,
     marginRight: 10,
+    backgroundColor: 'white'
   },
+  button: {
+    backgroundColor: '#43a047',
+    borderRadius: 25,
+    paddingVertical: 12,
+    paddingHorizontal: 25,
+    elevation: 5, // For shadow on Android
+    shadowColor: '#000', // For shadow on iOS
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    marginTop: 20
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center'
+  }
 });
-
